eac301c470ade08be91ca43e53aee9a139ed6fd9
Andy-xiong6 <xxxionghw@gmail.com>
2023-12-24 20:41:25 +0800

    Final finish

--- LinearAutoEncoder.py
+++ LinearAutoEncoder.py
@@ -4,14 +4,21 @@ class linearautoencoder:
     def __init__(self, hidden_layer_sizes ,input_size, output_size):
         self.hidden_layer_sizes = hidden_layer_sizes # a list of number. the length refers the layer numbers, and the element refers the hidden units
         self.weights = []
-        self.biases = []
-        self.weights = [np.random.randn(input_size, output_size)]
-        self.biases = [np.random.randn(1, output_size)]
-
+        self.bias = []
+        for i in range(len(hidden_layer_sizes)):
+            if i == 0:
+                self.weights.append(np.random.randn(input_size, hidden_layer_sizes[i]))
+                self.bias.append(np.random.randn(1, hidden_layer_sizes[i]))
+            else:
+                self.weights.append(np.random.randn(hidden_layer_sizes[i-1], hidden_layer_sizes[i]))
+                self.bias.append(np.random.randn(1, hidden_layer_sizes[i]))
+        self.weights.append(np.random.randn(hidden_layer_sizes[-1], output_size))
+        self.bias.append(np.random.randn(1, output_size))
+        
     def forward_propagation(self, X):
         activations = [X]
         for i in range(len(self.weights)):
-            z = activations[-1] @ self.weights[i] + self.biases[i]
+            z = activations[-1] @ self.weights[i] + self.bias[i]
             a = z # linear activation function
             activations.append(a)
         return activations
@@ -37,5 +44,5 @@ class linearautoencoder:
                 dW = activations[i].T @ deltas[i]
                 dB = np.sum(deltas[i], axis=0, keepdims=True)
                 self.weights[i] -= learning_rate * dW
-                self.biases[i] -= learning_rate * dB
+                self.bias[i] -= learning_rate * dB
     
--- main.py
+++ main.py
@@ -21,27 +21,19 @@ def precision(X):
     print("cluster 1: ", precision_cluster1)
     print("cluster 2: ", precision_cluster2)
     print("cluster 3: ", precision_cluster3)
-
-def plot(array, title):
-    # plot the loss vs iteration
-    plt.plot(array)
-    plt.xlabel('iteration')
-    plt.ylabel('loss')
-    plt.title(title)
-    plt.show()
     
 # KMeans algorithm and Soft KMeans algorithm
 print("KMeans with k = 3: ")
 kmeans = KMeans(3, 10)
 X_norm = kmeans.normalize(X)
 kmeans.train(X)
-plot(kmeans.array, "KMeans with k = 3")
+
 print("precision: ")
 precision(kmeans.predict(X))
 print("Soft KMeans with k = 3: ")
 softkmeans = SoftKMeans(3, 10, 10)
 softkmeans.train(X)
-plot(softkmeans.array, "Soft KMeans with k = 3")
+
 print("precision: ")
 precision(softkmeans.predict(X))
 print("J: ", softkmeans.J(X))
@@ -70,7 +62,7 @@ plt.legend()
 plt.show()
 
 #Soft KMeans with k=10
-softkmeans = SoftKMeans(10, 25, 15)
+softkmeans = SoftKMeans(10, 15, 15)
 softkmeans.train(X)
 # modified Soft KMeans algorithm with K = 10
 skmeans_modified = SoftKMeans(10, 25, 15)
@@ -84,11 +76,29 @@ plt.title('Soft KMeans and modified Soft KMeans with k = 10')
 plt.legend()
 plt.show()
 
+# plot the kmeans with k=10 and soft kmeans with k = 10 in a figure
+plt.plot(kmeans.array, label='KMeans')
+plt.plot(softkmeans.array, label='Soft KMeans')
+plt.xlabel('iteration')
+plt.ylabel('loss')
+plt.title('KMeans and Soft KMeans with k = 10')
+plt.legend()
+plt.show()
+
+#plot the modified kmeans and modified soft kmeans with k=10 in a figure
+plt.plot(kmeans_modified.array, label='modified KMeans')
+plt.plot(skmeans_modified.array, label='modified Soft KMeans')
+plt.xlabel('iteration')
+plt.ylabel('loss')
+plt.title('modified KMeans and modified Soft KMeans with k = 10')
+plt.legend()
+plt.show()
+
 # PCA algorithm
 raw_image = Image.open('flower.png')
 image = np.array(raw_image)
 image = image/255  # normalize
-pca = PCA(30)
+pca = PCA(60)
 Z = pca.train(image)
 recover_image = pca.recover(Z)
 plt.imshow(image, cmap='gray')
@@ -100,7 +110,7 @@ plt.show()
 
 # KMeans on image
 print("KMeans on image: ")
-kmeans = KMeans(50, 30)
+kmeans = KMeans(60, 100)
 X_norm = kmeans.normalize(image)
 kmeans.train(image)
 recover_image = kmeans.centroids[kmeans.predict(image)]
@@ -110,7 +120,7 @@ plt.show()
 
 # Linear Auto Encoder
 lae = linearautoencoder([30], 100, 100)
-lae.train(image, image, 0.001, 5000)
+lae.train(image, image, 0.00001, 1000000)
 print("J: ", lae.loss(image, image))
 recover_image = lae.forward_propagation(image)[-1]
 plt.imshow(recover_image, cmap='gray')
